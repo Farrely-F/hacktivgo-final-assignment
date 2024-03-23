9c85adafdf60cd3eab9dc2273185bc7b1562da22
farrely-f <farrely.firenza@outlook.com>
2024-03-24 05:20:09 +0700

    tweak db connection

--- db/db.go
+++ db/db.go
@@ -2,12 +2,13 @@ package db
 
 import (
 	"fmt"
-	"hacktiv-go/final-project-test/models"
 	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"hacktiv-go/final-project-test/models"
 )
 
 var (
@@ -17,7 +18,8 @@ var (
 func Connect() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		panic("Error loading .env file")
+		fmt.Println("Error loading .env file:", err)
+		return
 	}
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
@@ -29,17 +31,18 @@ func Connect() {
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		panic("Failed to connect to database")
-	} else {
-		fmt.Println("Database connection successful")
+		fmt.Println("Failed to connect to database:", err)
+		return
 	}
+
+	fmt.Println("Database connection successful")
 
 	// AutoMigrate your models here
 	err = db.AutoMigrate(&models.User{}, &models.Photo{}, &models.Comment{}, &models.SocialMedia{})
 	if err != nil {
-		panic("Failed to AutoMigrate models")
+		fmt.Println("Failed to AutoMigrate models:", err)
+		return
 	}
 
 	DB = db
